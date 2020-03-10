dc81c77dfd20dd90d94a4fcb545a104f9952bbf3
Dwika Ayu Novianti <you@example.com>
2020-03-11 02:59:28 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +11,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return view('coming-soon');
 });
+
+Route::match(['get', 'post'], '/admin','AdminController@login');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']],function(){
+	Route::get('/admin/dashboard','AdminController@dashboard');	
+	Route::get('/admin/settings','AdminController@settings');
+	Route::get('/admin/check-pwd','AdminController@chkPassword');
+	Route::match(['get','post'],'/admin/update-pwd','AdminController@updatePassword');
+
+	// Categories Routes (Admin)
+	Route::match(['get','post'],'/admin/add-category','CategoryController@addCategory');
+	Route::match(['get','post'],'/admin/edit-category/{id}','CategoryController@editCategory');
+	Route::match(['get','post'],'/admin/delete-category/{id}','CategoryController@deleteCategory');
+	Route::get('/admin/view-categories','CategoryController@viewCategories');
+
+	// Products Routes
+	Route::match(['get','post'],'/admin/add-product','ProductsController@addProduct');
+	Route::get('/admin/view-products','ProductsController@viewProducts');
+});
+
+
+Route::get('/logout','AdminController@logout');
+
+
+
