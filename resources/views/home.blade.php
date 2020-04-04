b5a7bb86fcd2bb62e7df0b31dbf45cd82d425b89
Dwika Ayu Novianti <you@example.com>
2020-04-04 23:59:10 +0700

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
-@extends('layouts.app')
-
+@extends('layouts.adminLayout.admin_design')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<!--main-container-part-->
+<div id="content">
+<!--breadcrumbs-->
+  <div id="content-header">
+    
+<!--End-breadcrumbs-->
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,11 @@ Route::group(['middleware' => ['auth']],function(){
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
